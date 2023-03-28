a9eb461deb1b0ed5a70acc0857e8537392181e80
Harsh <harshvegad33@gmail.com>
2023-03-28 12:59:47 +0530

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ExportPDF;
+use App\Http\Controllers\TaskExportPDF;
+use App\Http\Controllers\UserExportPDF;
 use App\Http\Livewire\Admin\Auth\ChangePassword;
 use App\Http\Livewire\Admin\Auth\Login;
 use App\Http\Livewire\Admin\Category;
@@ -29,5 +32,8 @@ Route::middleware(['auth:admin'])->group(function () {
         Route::get('/users', User::class)->name('admin.users');
         Route::get('/change-password', ChangePassword::class)->name('admin.changePassword');
         Route::get('/update-profile', UpdateProfile::class)->name('admin.updateProfile');
+        Route::get('/pdf', [ExportPDF::class, 'exportPDF'])->name('admin.exportPDF');
+        Route::get('/pdf', [TaskExportPDF::class, 'exportPDF'])->name('admin.taskexportPDF');
+        Route::get('/pdf', [UserExportPDF::class, 'exportPDF'])->name('admin.userexportPDF');
     });
 });
